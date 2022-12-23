e2b4bb0aef9c8f17d53f83b7670a3f5e5603cb1d
Kieran Connell <kieran@kconnell.co.uk>
2022-12-23 23:58:19 +0000

    Make reciprocal table configurable.
    And check for division out of range in _DEBUG.

--- lib/divide.asm
+++ lib/divide.asm
@@ -2,6 +2,35 @@
 ; Division routines.
 ; ============================================================================
 
+; x = 0.0 to 256.0 [1 to 1<<8]
+; There are 1<<16 table entries so x<<(16-8) = x<<8
+; Table is 1<<24 / x<<8 = (1/x) << 16
+
+; If want x = 0.0 to 1024.0 [1 to 1<<10]
+; There are 1<<16 table entries so x<<(16-10) = x<<6
+; Then table is 1<<22 / x<<6 = (1/x) << 16
+
+; If want x = 0.0 to 1024.0 [1 to 1<<10]
+; There are 1<<12 table entries so x<<(12-10) = x<<2
+; Then table is 1<<(16+2) / x<<2 = (1/x) << 16
+
+; If want x = 0.0 to M [1 to 1<<m]
+; There are 1<<t table entries.
+; Shift for x, s=(t-m) so x<<(t-m) in the table.
+; If maths precision is 1<<p.
+; Then table is 1<<(p+s) / x<<s.
+
+; If want x = 0.0 to 1024.0 [m=10]
+; But only a small table, say 256 entries, i.e. t=8
+; Then shift s=(8-10) = -2. So x>>2 in the table.
+; Table is (1<<p+s) / x<<s = 1<<14 / x>>2
+
+
+.equ Reciprocal_t, 16           ; Table entries = 1<<t
+.equ Reciprocal_m, 9            ; Max value = 1<<m
+.equ Reciprocal_s, Reciprocal_t-Reciprocal_m    ; Table is (1<<16+s)/(x<<s)
+
+
 ; Divide R0 by R1
 ; Parameters:
 ;  R0=numerator  [s15.16]       ; (a<<16)
@@ -9,16 +38,6 @@
 ; Trashes:
 ;  R8-R10
 divide:
-    ; Limited precision.
-    mov r1, r1, asr #10         ; [16.6] (b<<6)
-
-    CMP R1,#0                   ; Test for division by zero
-    ADREQ R0,divbyzero          ; and flag an error
-    SWIEQ OS_GenerateError      ; when necessary
-
-    cmp r0, #0                  ; Test if result is zero
-    moveq pc, lr
-
     ; Signed division - any better way to do this?
     eor r10, r0, r1             ; R0 eor R1 indicates sign of result
     cmp r0, #0
@@ -27,16 +46,45 @@ divide:
     rsbmi r1, r1, #0            ; make positive  
 
     .if _USE_RECIPROCAL_TABLE
+    mov r1, r1, asr #16-Reciprocal_s    ; [16.6]    (b<<s)
+
+    .if _DEBUG
+    cmp r1,#0                   ; Test for division by zero
+    adreq r0,divbyzero          ; and flag an error
+    swieq OS_GenerateError      ; when necessary
+    .endif
+
+    cmp r0, #0                  ; Test if result is zero
+    moveq pc, lr
+
     adr r9, reciprocal_table
-    bic r8, r1, #0xff0000       ; [10.6]    (b<<6)
-    ldr r8, [r9, r8, lsl #2]    ; [0.16]    (1<<22)/(b<<6) = (1<<16)/b
-    mov r0, r0, asr #10         ; [16.6]    (a<<6)
-    mul r9, r0, r8              ; [10.22]   (a<<6)*(1<<16)/b = (a<<22)/b
-    mov r9, r9, asr #6          ; [10.16]   (a<<16)/b = (a/b)<<16
+
+    .if _DEBUG
+    ; Limited precision.
+    cmp r1, #1<<Reciprocal_t    ; Test for numerator too large
+    adrge R0,divrange           ; and flag an error
+    swige OS_GenerateError      ; when necessary
+    .endif
+
+; TODO: These bits should be cleared by shifting left if necessary.
+;   bic r1, r1, #0xff0000       ; [10.6]    (b<<6)
+    ldr r1, [r9, r1, lsl #2]    ; [0.16]    (1<<16+s)/(b<<s) = (1<<16)/b
+
+    mov r0, r0, asr #16-Reciprocal_s    ; [16.6]    (a<<s)
+    mul r9, r0, r1                      ; [10.22]   (a<<s)*(1<<16)/b = (a<<16+s)/b
+    mov r9, r9, asr #Reciprocal_s       ; [10.16]   (a<<16)/b = (a/b)<<16
     .else
 
     ; Limited precision.
-    mov r0, r0, asl #8          ; [8.16]
+    ; TODO: Make configurable - fixed to m=10, i.e. max value=1024.
+    mov r1, r1, asr #10         ; [16.6]    (b<<6)
+    CMP R1,#0                   ; Test for division by zero
+    ADREQ R0,divbyzero          ; and flag an error
+    SWIEQ OS_GenerateError      ; when necessary
+
+    mov r0, r0, asl #6          ; [10.22]   (a<<22)
+    cmp r0, #0                  ; Test if result is zero
+    moveq pc, lr
 
     ; Taken from Archimedes Operating System, page 28.
     MOV R8, #1
@@ -57,7 +105,7 @@ divide:
     MOV R1,R1,LSR #1
     MOVS R8,R8,LSR #1
     BCC .3
-
+                                ; (a<<22) / (b<<6) = (a/b) << 16
     .endif
 
     movs r10, r10               ; get sign back
@@ -74,13 +122,11 @@ divide:
 	.align 4
 	.long 0
 
-; x = 0.0 to 256.0 [1 to 1<<8]
-; There are 1<<16 table entries so x<<(16-8) = x<<8
-; Table is 1<<24 / x<<8 = (1/x) << 16
-
-; If want x = 0.0 to 1024.0 [1 to 1<<10]
-; There are 1<<16 table entries so x<<(16-10) = x<<6
-; Then table is 1<<22 / x<<6 = (1/x) << 16
+    divrange: ;The error block
+    .long 18
+	.byte "Division out of range"
+	.align 4
+	.long 0
 
 .if _USE_RECIPROCAL_TABLE
 ; Trashes: r0-r2, r8-r9, r12
@@ -91,7 +137,7 @@ MakeReciprocal:
  
     mov r2, #1
 .4:
-    mov r0, #1<<22
+    mov r0, #1<<(16+Reciprocal_s)       ; assumes PRECISION_BITS==16
     mov r1, r2
 
     ; Taken from Archimedes Operating System, page 28.
@@ -116,7 +162,7 @@ MakeReciprocal:
 
     str r9, [r12], #4
     add r2, r2, #1
-    cmp r2, #1<<16
+    cmp r2, #1<<Reciprocal_t
     blt .4
 
     mov pc, lr
