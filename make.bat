93728f2741be0f4eed7d24989a3124d45b89e81a
Kieran Connell <kieran@kconnell.co.uk>
2022-12-30 18:08:01 +0000

    Shelf of attempting to compress the exe.
    Funk knows what's going on here.
    LZ4 decompressor does a prefetch abort.
    Shrinkler decompressor not responsive.
    Must be doing something stupid but need to get the prod uploaded.
    Let's consider this a party version and can sort it all out another time.

--- make.bat
+++ make.bat
@@ -12,6 +12,18 @@ if %ERRORLEVEL% neq 0 (
 	exit /b 1
 )
 
+echo Assembling loader...
+bin\vasmarm_std_win32.exe -L build\loader.txt -m250 -Fbin -opt-adr -o build\loader.bin lib\loader.asm
+
+if %ERRORLEVEL% neq 0 (
+	echo Failed to assemble code.
+	exit /b 1
+)
+
+echo Compress exe...
+rem bin\lz4.exe -f build\proto-arc.bin build\proto-arc.lz4
+bin\Shrinkler.exe -d -b -p -z build\proto-arc.bin build\proto-arc.shri
+
 echo Making !folder...
 set FOLDER="!Tipsy"
 if EXIST %FOLDER% del /Q "%FOLDER%"
@@ -19,7 +31,8 @@ if NOT EXIST %FOLDER% mkdir %FOLDER%
 
 echo Adding files...
 copy folder\*.* "%FOLDER%\*.*"
-copy build\proto-arc.bin "%FOLDER%\!RunImage,ff8"
+copy build\loader.bin "%FOLDER%\!RunImage,ff8"
+copy build\proto-arc.shri "%FOLDER%\Demo,ffd"
 copy "data\music\arcchoon.mod" "%FOLDER%\Music,001"
 
 echo Copying !folder...
